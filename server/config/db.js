f948063fc59c06fac9da00fcbaa770c37b238470
Sumitdurgam <sumitdurgam12@gmail.com>
2026-08-13 14:04:42 +0530

    update url

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://sumitdurgam17_db_user:<db_password>@cluster0.dutcqi4.mongodb.net/?appName=Cluster0');
+    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://sumitdurgam17_db_user:X2R2VuIQlbWuCtfw@cluster0.3perepq.mongodb.net');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB Connection Error: ${error.message}`);
